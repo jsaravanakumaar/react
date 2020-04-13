86ec7b741c6b641ccbffdfb9ede5da003af45b3d
Saravanakumaar Jeyabalan <jsarava@gmail.com>
2020-04-13 16:29:12 +0530

    Initial commit

---
+++ src/components/MainWithClass.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import ThemeContext from '../context/ThemeContext';
+import AppTheme from '../Colors';
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  static contextType = ThemeContext;
+
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {
+          ([theme]) => {
+            const currentTheme = AppTheme[theme];
+            return (
+              <main style={{
+                padding: "1rem",
+                backgroundColor: `${currentTheme.backgroundColor}`,
+                color: `${currentTheme.textColor}`,
+                }}>
+
+                <h1>Heading 1</h1>
+                <p>Please click icons to switch Themes</p>
+                <button>This is a Button</button>        
+              </main>
+            );
+          }
+        }
+      </ThemeContext.Consumer>
+    );
+  }
+}
+export default Main;
